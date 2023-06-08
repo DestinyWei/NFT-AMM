ffe7de6da38189f6833d0bc712c03a18e7ced481
周哲 <1708834102@qq.com>
2023-06-08 19:47:41 +0800

    Configure testing smartcontract environment, Industrial standard

--- test/foundry/NFTAMM.t.sol
+++ test/foundry/NFTAMM.t.sol
@@ -2,31 +2,17 @@
 
 pragma solidity ^0.8.17;
 
-import "forge-std/Test.sol";
-import "../../contracts/NFTAMM.sol";
-import "../../contracts/WETH.sol";
-import "../../contracts/fundContract.sol";
-import "../../contracts/Calculate.sol";
-import "../../contracts/Fragmentation.sol";
+import "./utils/NFT-AMMTestBase.sol";
 
-contract NFTAMMTest is Test {
-    WETH weth;
-    Fragmentation fragmentation;
-    FTCalculate calculate;
-    FundContract fund;
-    NFTAMM nftAMM;
+contract NFTAMMTest is NFTAMMTestBase {
 
     function _setUp() public {
-        weth = new WETH();
-        fragmentation = new Fragmentation();
-        calculate = new FTCalculate();
-        fund = new FundContract();
-        nftAMM = new NFTAMM(address(weth), address(fragmentation), address(calculate), address(fund));
-
+        setUp();
     }
 
     function test_NA_Deployment() public {
         assertEq(nftAMM.ONE_ETH(), 10 ** 18);
+
     }
 
     function test_NA_addLiquidityWithETH_Success() public {
---
+++ test/foundry/utils/import.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import {Utils} from "./Utils.sol";
+import "../../../contracts/NFTAMM.sol";
+import "../../../contracts/WETH.sol";
+import "../../../contracts/fundContract.sol";
+import "../../../contracts/Calculate.sol";
+import "../../../contracts/Fragmentation.sol";
+import "../../../contracts/ammPeriphery.sol";
+import "../../../contracts/DexToken.sol";
+import "../../../contracts/NFT.sol";
+import "../../../contracts/FT.sol";
+import "../../../contracts/LPToken.sol";
+//import "../../../contracts/Pool.sol";
+
+contract Imports {
+
+}
